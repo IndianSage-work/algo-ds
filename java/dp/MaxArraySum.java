173be21285fb594bd764c72615ead21e16b8c03b
IndianSage-work <indiansage.work@gmail.com>
2020-01-31 13:15:18 +0530

    Update MaxArraySum.java

--- java/dp/MaxArraySum.java
+++ java/dp/MaxArraySum.java
@@ -20,11 +20,13 @@ public class Solution {
             arr[i] = Math.max(arr[i-1], arr[i]+arr[i-2]);
         return arr[arr.length-1];
         /*
-        Firstly, my assumption that you won't use this array again is significant, as this solution overwrites the initial values. You can circumvent this by simply using a temporary array to store updated values.
+        Firstly, my assumption that you won't use this array again is significant, as this solution overwrites the initial values. 
+        You can circumvent this by simply using a temporary array to store updated values.
 
         arr[0] = Math.max(0, arr[0]);
 
-        This is where my assumption that the empty set is a valid subset comes into play. Were we to only consider subsets of size >= 1, we'd have to concern ourselves with finding the least negative number in an array of purely negative numbers. However, since we can use the empty set, 0 is a valid sum for an array of purely negative integers.
+        This is where my assumption that the empty set is a valid subset comes into play. Were we to only consider subsets of size >= 1, we'd have to concern ourselves with finding the least negative number in an array of purely negative numbers.
+        However, since we can use the empty set, 0 is a valid sum for an array of purely negative integers.
 
         arr[1] = Math.max(arr[0], arr[1]);
 
@@ -32,7 +34,9 @@ public class Solution {
 
         for (int i = 2; i < arr.length; i++) arr[i] = Math.max(arr[i-1], arr[i]+arr[i-2]);
 
-        This is where the magic happens. Essentially, we sum from arr[0] up to arr[n-1]. Since we're only concerned with non-sequential subsets, we must choose if we want the optimal sum of the previous index, arr[i-1], or to keep the current index's value and the optimal sum of non-sequential values before it, arr[i]+arr [i-2]. Note that at this point, arr[i-1] and arr[i-2] store the optimal sums possible at those two locations, not their original values.
+        This is where the magic happens. Essentially, we sum from arr[0] up to arr[n-1]. 
+        Since we're only concerned with non-sequential subsets, we must choose if we want the optimal sum of the previous index, arr[i-1], or to keep the current index's value and the optimal sum of non-sequential values before it, arr[i]+arr [i-2]. 
+        Note that at this point, arr[i-1] and arr[i-2] store the optimal sums possible at those two locations, not their original values.
 
         return arr[arr.length-1];
 
